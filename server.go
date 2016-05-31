7e803048bd26c26e9d6104bd57405083c6d53653
ashu01 <as01hu@gmail.com>
2016-05-31 13:50:03 +0530

    Modules for different instances added to server.go

--- server.go
+++ server.go
@@ -50,11 +50,15 @@ func main() {
 		c.JSON(200, "ok")
 	})
 
+	var EmailBefore string
+
 	r.POST("/createuserid", func(c *gin.Context) {
 		var request UserIDCreate
 		c.BindJSON(&request)
 
-		fmt.Println("\n\nEmail Received : %#v\n\n", request)
+		fmt.Println("\n\nRequest Received : \n\n", request)
+
+		EmailBefore = request.Email
 
 		tx, _ := db.Begin() // tx => transaction , _ => error and execute
 
@@ -65,6 +69,7 @@ func main() {
 		tx.QueryRow(`
         INSERT INTO users (username, name, email) values ($1, $2, $3) returning userid
     `, request.UserName, request.Name, request.Email).Scan(&response.Userid)
+
 		commit_err := tx.Commit()
 
 		if commit_err != nil {
@@ -89,6 +94,9 @@ func main() {
 			return
 		}
 
+		// fmt.Printf("\n\n  User : %v \n\n", userid.Userid)
+		// fmt.Println()
+
 		var email string
 		db.QueryRow(`
 			SELECT email 
@@ -103,10 +111,122 @@ func main() {
 			return
 		}
 
-		email_map := make(map[string]string)
-		email_map["email"] = email
-		c.JSON(200, email_map)
+		if email != EmailBefore {
+			responsefail := make(map[string]string)
+			responsefail["error"] = "can't generate user"
+			c.JSON(405, responsefail)
+			return
+		}
+
+		fmt.Printf("\n\nUserid allocated to corresponding Email\n\n")
+
+		emailMap := make(map[string]string)
+		emailMap["email"] = email
+		c.JSON(200, emailMap)
 	})
+
+	// here we have to run dummy_dumpdataintousers.py that is in API folder
+	r.POST("/dumpdataintousers", func(c *gin.Context) {
+		var userdata UserIDCreate
+		c.BindJSON(&userdata)
+
+		fmt.Println("\n\nRequest Received : \n\n", userdata)
+
+		tx, _ := db.Begin() // tx => transaction , _ => error and execute
+
+		// insert into users table
+		db.QueryRow(`
+        INSERT INTO users (username, name, email) VALUES ($1, $2, $3)
+    `, userdata.UserName, userdata.Name, userdata.Email)
+
+		commitErr := tx.Commit()
+
+		if commitErr != nil {
+			tx.Rollback()
+			c.JSON(500, "ERR")
+			return
+		}
+
+		defer tx.Rollback() // it will be executed after the completion of local function
+
+		c.JSON(200, "ok") // Successfully inserted into users table
+
+	})
+
+	// dump data into usersdescription
+	r.POST("/dumpintousersdescription", func(c *gin.Context) {
+		var request DumpUsersdescription
+		c.BindJSON(&request)
+		fmt.Printf("\n\n Received : %#v\n\n", request)
+
+		tx, _ := db.Begin() // tx => transaction , _ => error and execute
+
+		//fmt.Println("Before Query")
+		db.QueryRow(`
+        INSERT INTO usersdescription (userid, deviceid, platform) VALUES ($1, $2, $3)
+    `, request.UserID, request.DeviceID, request.Platform)
+
+		//fmt.Println("After Query")
+		commitErr := tx.Commit()
+
+		fmt.Println("COMMIT ERROR : ", commitErr)
+
+		if commitErr != nil {
+			tx.Rollback()
+			c.JSON(500, "ERR")
+			return
+		}
+		defer tx.Rollback() // it will be executed after the completion of local function
+
+		var response Response
+		response.Timestamp = time.Now().Unix()
+
+		c.JSON(200, response)
+
+	})
+
+	// apply query to get deviceid and platform
+	r.POST("/findUseridPlatform", func(c *gin.Context) {
+		var request UserIDCreate
+		// Dump the incoming request data into a struct variable
+		c.BindJSON(&request)
+		fmt.Println("\n\n\n Received : %#v\n\n\n", request)
+		// database transaction begins
+
+		tx, _ := db.Begin() // tx => transaction , _ => error and execute
+
+		defer tx.Rollback() // it will be executed after the completion of local function
+
+		//var userid int64 //variable for storing user_id
+		var deviceid string
+		var platform string
+
+		err := db.QueryRow(`
+                SELECT usersdescription.deviceid, usersdescription.platform
+                FROM users, usersdescription
+                WHERE usersdescription.userid = users.userid AND username = $1 AND email = $2
+        `, request.UserName, request.Email).Scan(&deviceid, &platform)
+
+		fmt.Println("\n\nDeviceid and platform : ", deviceid, platform)
+		fmt.Println(err)
+
+		commit_err := tx.Commit() // commit the transaction
+
+		if commit_err != nil {
+			tx.Rollback()
+			c.JSON(500, "Duplicate Elements") // commit error should be mentioned according to the corresponding errors
+			return
+		}
+
+		var response ToPNS
+
+		response.DeviceID = deviceid
+		response.Platform = platform
+		fmt.Printf("\n\n\n Response :  %#v\n\n\n", response)
+
+		c.JSON(200, response)
+	})
+
 	fmt.Printf("Going live on :7000")
 	r.Run(":7000")
 }
@@ -142,4 +262,15 @@ type UserIDCreate struct {
 
 type UserIDResp struct {
 	Userid int64 `json:"userid,omitempty"`
+}
+
+type DumpUsersdescription struct {
+	UserID   int64  `json:"userid,omitempty"`
+	DeviceID string `json:"deviceid,omitempty"`
+	Platform string `json:"platform,omitempty"`
+}
+
+type ToPNS struct {
+	DeviceID string `json:"deviceid,omitempty"`
+	Platform string `json:"platform,omitempty"`
 }
